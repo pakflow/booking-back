c446f9ce7d25bb3e8920fbd43abb65c979e70607
Maxim <bboy.mars.97@gmail.com>
2025-01-10 17:23:23 +0600

    Revert "Merge pull request #1 from pakflow/feature/auth"
    This reverts commit 86291030797026bff2b8a2cad59568cd4f68d6d5, reversing
    changes made to 7867a39eec80e0cfd1a1f22445af72766db629ac.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './modules/user/user.module';
-import { User } from './modules/user/entities/user.entity';
-import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '050997',
-      database: 'booking',
-      entities: [User],
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    UserModule,
-    AuthModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
